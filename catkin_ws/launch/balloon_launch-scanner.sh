06f34a9728202d66a38ae472cfb13af43e4cfed9
Austin <austin.m.anderson@colorado.edu>
2015-12-15 17:33:49 -0700

    distinguish two startup scripts

---
+++ catkin_ws/launch/balloon_launch-scanner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ../devel/setup.bash
+roslaunch mavros px4.launch &
+sleep 10s
+rosrun temp_mon temp_mon_node &
+sleep 2s
+rosrun ros_sdr hackrf_sdr &
+sleep 5s
+roslaunch ros_sdr sdr_rec.launch & 
+sleep 5s
+rosrun ros_sdr scanner_tuner.py &
